ef52b6519b94a63f7742063ad9345f2cd26ac6be
Matt Gerst <mattgerst@gmail.com>
2017-10-24 19:50:04 -0500

    Refactor the tests to use a fixture for the pencil object

--- tests/test_pencil.py
+++ tests/test_pencil.py
@@ -1,14 +1,19 @@
+import pytest
+
 from pencil import Pencil
 
 
-def test_pencil_writes_to_paper():
-    pencil = Pencil()
+@pytest.mark.fixture
+def pencil():
+    return Pencil()
+
+
+def test_pencil_writes_to_paper(pencil):
     pencil.write("She sells sea shells")
     assert pencil.paper == "She sells sea shells"
 
 
-def test_pencil_writes_to_paper_twice():
-    pencil = Pencil()
+def test_pencil_writes_to_paper_twice(pencil):
     pencil.write("She sells sea shells")
     pencil.write(" down by the sea shore")
     assert pencil.paper == "She sells sea shells down by the sea shore"
