644f120503d5732819ae29a474b18cf87defa774
Matt Gerst <mattgerst@gmail.com>
2017-10-26 12:26:23 -0500

    Add some docstrings.

--- pencil/pencil.py
+++ pencil/pencil.py
@@ -1,4 +1,7 @@
 class Pencil:
+    """
+    Represents a pencil that can write and erase messages.
+    """
     DEFAULT_DURABILTY = 50
     ERASER_DURABILTY = 50
 
@@ -10,6 +13,18 @@ class Pencil:
         self.eraser_durability = eraser_durability
 
     def write(self, message):
+        """
+        Write a message to the paper.
+
+        Writing text will consume durability with the
+        following rules:
+
+        * spaces are free
+        * uppercase costs 2
+        * everything else costs 1
+
+        :param message: The message to write
+        """
         for char in message:
             cost = 1
             if self.durability == 0:
@@ -24,6 +39,13 @@ class Pencil:
             self.durability -= cost
 
     def sharpen(self):
+        """
+        Sharpen the pencil.
+
+        If then pencil has run out of length, this will
+        do nothing. Otherwise this will reset the
+        durability and consume one unit of length.
+        """
         if self.length == 0:
             return
 
@@ -31,6 +53,11 @@ class Pencil:
         self.length -= 1
 
     def erase(self, text):
+        """
+        Erase the last occurrence of the given text.
+
+        :param text: The text to erase
+        """
         start_index = self.paper.rfind(text)
         end_index = start_index + len(text)
 
@@ -51,6 +78,15 @@ class Pencil:
             self.eraser_durability -= cost
 
     def edit(self, text, offset):
+        """
+        Edit text on the paper at a given offset.
+
+        The written text will consume the pencil's durability the same
+        as using :py:meth:`Pencil.write`.
+
+        :param text: The text to write on the paper
+        :param offset: The zero-indexed offset to start writing at
+        """
         end_index = offset + len(text)
 
         replacement = ""
